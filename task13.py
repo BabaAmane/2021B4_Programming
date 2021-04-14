6b00d936eb25715f2b3fdebb0699a3370ab059a1
BabaAmane <amane88@keio.jp>
2021-04-15 08:36:16 +0900

    task13

---
+++ task13.py
@@ -0,0 +1,16 @@
+def tf(term, doc):
+    count = 0
+    for i in range(len(doc)):
+        if term == doc[i]:
+            count += 1
+    return count / len(doc)
+
+
+
+
+docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
+terms = ["リンゴ", "レモン", "ミカン"]
+
+for term in terms:
+    for doc in docs:
+        print("tf(",term, doc,")=", tf(term, doc))
