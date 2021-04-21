04bc899e2b565432613d0e826f879033f9b2fffe
BabaAmane <amane88@keio.jp>
2021-04-21 20:45:31 +0900

    task1

--- day2/task1_2.py
+++ day2/task1_2.py
@@ -3,7 +3,7 @@ import task1_1
 if __name__=='__main__':
     AND = task1_1.Perception(1, 1, 2)
     NAND = task1_1.Perception(-1, -1.5, -2)
-    XOR = task1_1.Perception(1, 1, 0.5)
+    OR = task1_1.Perception(1, 1, 0.5)
 
     x1_list = [1, 1, 0, 0]
     x2_list = [1, 0, 1, 0]
@@ -11,7 +11,7 @@ if __name__=='__main__':
     for i in range(len(x1_list)):
          print("AND(%d, %d) = %d " %(x1_list[i],x2_list[i],AND.forward(x1_list[i],x2_list[i])) , end = '')
          print("NAND(%d, %d) = %d " %(x1_list[i],x2_list[i],NAND.forward(x1_list[i],x2_list[i])) , end = '')
-         print("OR(%d, %d) = %d " %(x1_list[i],x2_list[i],XOR.forward(x1_list[i],x2_list[i])))
+         print("OR(%d, %d) = %d " %(x1_list[i],x2_list[i],OR.forward(x1_list[i],x2_list[i])))
 
 
     
