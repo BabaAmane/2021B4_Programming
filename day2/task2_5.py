6f4e708dc0919ed1846f9e81f51eaf5c1081bcf2
BabaAmane <amane88@keio.jp>
2021-04-23 14:18:07 +0900

    ta

--- day2/task2_5.py
+++ day2/task2_5.py
@@ -1,5 +1,12 @@
 import numpy as np
 import task2_2
+##うまくいかない
+
+def softmax(x):
+        c = np.max(x)
+        exp_x = np.exp(x - c)
+        sum_exp_x = np.sum(np.exp(x - c), axis=1, keepdims=True)
+        return exp_x / sum_exp_x
 
 class MLP_3Layer:
 
@@ -11,7 +18,7 @@ class MLP_3Layer:
         self.network['b1'] = b1
         self.network['b2'] = b2
         self.network['b3'] = b3
-        
+
     def forward(self,x):
         W1, W2, W3 = self.network['W1'], self.network['W2'], self.network['W3']
         b1, b2, b3 = self.network['b1'], self.network['b2'], self.network['b3']
@@ -21,10 +28,11 @@ class MLP_3Layer:
         
         a2 = np.dot(z1, W2) + b2
         z2 = task2_2.relu(a2)
-        
+
         a3 = np.dot(z2, W3) + b3
-        return a3
-  
+        z3 = softmax(a3)
+        return z3
+
 if __name__=='__main__':
     x = np.array([[1.0, 0.5], [-0.3, -0.2], [0.0, 0.8], [0.3, -0.4]])
     W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
