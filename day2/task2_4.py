7b43eed46049fa2da27a8fbb8f0da57ceecec883
BabaAmane <amane88@keio.jp>
2021-04-23 11:10:25 +0900

    task

---
+++ day2/task2_4.py
@@ -0,0 +1,38 @@
+import numpy as np
+import task2_2
+
+class MLP_3Layer:
+
+    def __init__(self, W1, b1, W2, b2, W3, b3):
+        self.network={}
+        self.network['W1'] = W1
+        self.network['W2'] = W2
+        self.network['W3'] = W3
+        self.network['b1'] = b1
+        self.network['b2'] = b2
+        self.network['b3'] = b3
+        
+    def forward(self,x):
+        W1, W2, W3 = self.network['W1'], self.network['W2'], self.network['W3']
+        b1, b2, b3 = self.network['b1'], self.network['b2'], self.network['b3']
+        
+        a1 = np.dot(x, W1) + b1
+        z1 = task2_2.relu(a1)
+
+        a2 = np.dot(z1, W2) + b2
+        z2 = task2_2.relu(a2)
+        
+        a3 = np.dot(z2, W3) + b3
+        return a3
+  
+if __name__=='__main__':
+    x = np.array([[1.0, 0.5], [-0.3, -0.2], [0.0, 0.8], [0.3, -0.4]])
+    W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    b1 = np.array([0.1, 0.2, 0.3])
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    b2 = np.array([0.1, 0.2])
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    b3 = np.array([0.1, 0.2])
+
+    network = MLP_3Layer(W1, b1, W2, b2, W3, b3)
+    print(network.forward(x))
